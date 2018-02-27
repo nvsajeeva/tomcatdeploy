77691d4fa74ecdebe112bd8235458c6a7fd813a4
satyashildeshpande <33680995+satyashildeshpande@users.noreply.github.com>
2018-02-27 14:43:35 +0530

    Update installtomcat.sh

--- scripts/installtomcat.sh
+++ scripts/installtomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install java-1.8.0
-sudo yum remove java-1.7.0-openjdk
+#sudo yum remove java-1.7.0-openjdk
 sudo yum install java-1.8.0-openjdk-devel.x86_64
 cd /opt
 wget http://www-eu.apache.org/dist/tomcat/tomcat-7/v7.0.85/bin/apache-tomcat-7.0.85.tar.gz
