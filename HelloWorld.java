17713d15c78be0f0d469b35157e0457786ec2f7c
satyashildeshpande <33680995+satyashildeshpande@users.noreply.github.com>
2018-03-02 07:59:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -18,7 +18,7 @@ public class HelloWorld extends HttpServlet
                 Printer.println("\t<title>CodeDeploy Test</title>");
                 Printer.println("</head>");
                 Printer.println("<body>");
-                Printer.println("<b>Hello World!</b>");
+                Printer.println("<b>Hello World!</b>")
                 Printer.println("<b>Now it's been updated from Github!</b>");
                 Printer.println("<b>This has to work now done few things!</b><br>");
                 Printer.println("<b>If not reflecting then should stop Deployment!</b>");
