7355739a99c9257f1bd9d841cd28da2bd0885bbd
satyashildeshpande <33680995+satyashildeshpande@users.noreply.github.com>
2018-03-02 06:55:24 +0530

    Update starttomcat.sh

--- scripts/starttomcat.sh
+++ scripts/starttomcat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh /usr/local/tomcat7/bin/startup.sh
+
 cd /usr/local/tomcat7/webapps/hello/WEB-INF/classes/
 javac -cp /usr/local/tomcat7/lib/servlet-api.jar HelloWorld.java
 sh /usr/local/tomcat7/bin/shutdown.sh
